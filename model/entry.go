e2daa01785c1ed7c2f749fc9bcf629e42c92a48c
dalmir <dalmir@silva.americas.hpqcorp.net>
2014-09-18 17:18:13 -0300

    sync

--- model/entry.go
+++ model/entry.go
@@ -1,73 +1,8 @@
 package model
 
-import (
-  "github.com/coopernurse/gorp"
-  "github.com/dalmirdasilva/gorecord/persistence"
-  "log"
-  "reflect"
-)
-
-type fn func(* gorp.DbMap) (interface{}, error)
-
 type Entry struct {
   Id int64
   Created int64
   Updated int64
-  Self interface{} `db:"-"`
-}
-
-
-
-func (e *Entry) Save() error  {
-  _, err := run(func(db *gorp.DbMap) (interface{}, error) {
-    var err error = nil
-    var rows int64 = 0
-    if e.Id == 0 {
-      err = db.Insert(e.Self)
-    } else {
-      rows, err = db.Update(e.Self)
-    }
-    return rows, err
-  })
-  return err
-}
-
-func (e *Entry) Delete() error  {
-  _, err := run(func(db *gorp.DbMap) (interface{}, error) {
-    var err error = nil
-    var rows int64 = 0
-    if e.Id != 0 {
-      rows, err = db.Delete(e)
-    }
-    return rows, err
-  })
-  return err
-}
-
-func (e Entry) Find(id int64) (*Entry, error)  {
-  log.Println("find with: ", reflect.TypeOf(e))
-  /*var result Entry
-  var ok bool
-  found, err := run(func(db *gorp.DbMap) (interface{}, error) {
-    var entry Entry
-    tableName := persistence.GetTableFrom(e)
-    db.SelectOne(&entry, "select * from " + tableName)
-    return entry, nil
-  })
-  if err != nil {
-    result, ok = found.(Entry)
-    if !ok {
-      err = errors.New("Cannot cast result to Entry.")
-    }
-  }
-  return &result, err
-  */
-  return nil, nil
-}
-
-func run(f fn) (interface{}, error) {
-  pdb := persistence.GetDatabase()
-  db := pdb.DbMap()
-  return f(db)
 }
 
--- persistence/database.go
+++ persistence/database.go
@@ -1,10 +1,6 @@
 package persistence
 
-import (
-  "github.com/coopernurse/gorp"
-  "reflect"
-  "log"
-)
+import "github.com/coopernurse/gorp"
 
 type Database interface {
   Initialize(settings map[string]string)
@@ -12,7 +8,6 @@ type Database interface {
 }
 
 var db Database
-var registeredTables map[string]string
 
 func GetDatabase() Database {
   if db == nil {
@@ -22,45 +17,26 @@ func GetDatabase() Database {
 }
 
 func Initialize(databaseName string, settings map[string]string) {
-  log.Println("Initializing database.")
   db = newDatabase(databaseName, settings)
-  log.Println(db)
 }
 
 func RegisterTables(tables map[string]interface{}) {
-  log.Println("Registering tables")
-  registeredTables = make(map[string]string)
   for tableName, object := range tables {
-    log.Println("Table registered: ", tableName)
     db.DbMap().AddTableWithName(object, tableName).SetKeys(true, "Id")
-    registeredTables[reflect.TypeOf(object).Name()] = tableName
   }
   db.DbMap().CreateTablesIfNotExists()
-  log.Println("Done, tables: ", registeredTables)
-}
-
-func GetTableFrom(entry interface{}) string {
-  for name, instance := range registeredTables {
-    if reflect.TypeOf(instance).Kind() ==  reflect.TypeOf(entry).Kind() {
-      return name
-    }
-  }
-  return "unknown"
 }
 
 func newDatabase(databaseName string, settings map[string]string) Database {
   var database Database
   if databaseName == "sqlite3" {
-    log.Println("Instantiating Sqlite")
     database = new(Sqlite)
   }
   if databaseName == "mysql" {
-    log.Println("Instantiating Mysql")
     database = new(Mysql)
   }
   if database != nil {
     database.Initialize(settings)
   }
-  log.Println("Database is: ", database)
   return database
 }
